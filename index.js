729c5904476da3e2b70ae6dd54e1f3107c5bf172
Hannah <hannahwoodward@hotmail.com>
2021-07-27 09:58:12 +0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+let result = returnFirstTwoDrivers(drivers);
+console.log("result1: ", result);
+
+let result1 = returnLastTwoDrivers(drivers);
+console.log("result2: ", result);
+
+//higher order function
+const createFareMultiplier = function (multiplier) {
+// example of closure
+return function (fare) {
+  return fare * multiplier;
+}
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+let totalFare = fareDoubler(30);
+console.log ('Total doubled fare: ', totalFare);
+
+let totalFare2 = fareTripler(30);
+console.log ('Total tripled fare: ', totalFare2);
+
+const selectDifferentDrivers = function (drivers,driversToReturn) {
+return driversToReturn(drivers);
+}
+let selectedDrivers = selectDifferentDrivers(drivers, returnLastTwoDrivers);
+console.log('selectedDrivers: ', selectedDrivers);
